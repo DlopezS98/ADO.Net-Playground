479b5f8a0d19c7b15fbb8f9dbfc63b4ab11b92d8
DlopezS98 <01dlopezs98@gmail.com>
2023-09-02 11:59:16 -0600

    Initial project setup

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.OleDb;
+using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        IConfigurationRoot config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        string connectionString = config.GetConnectionString("MSSQLServer") ?? string.Empty;
+        Console.WriteLine(connectionString);
+
+        //OleDbConnection connection = new OleDbConnection(connectionString);
+    }
+}

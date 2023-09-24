7b17359d7cbf49fcfc183a657450e5fbb786a085
DlopezS98 <01dlopezs98@gmail.com>
2023-09-24 16:37:34 -0600

    Single connection SQL

--- SingletonSqlConnection.cs
+++ SingletonSqlConnection.cs
@@ -1,12 +1,19 @@
-﻿namespace core_data_provider;
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace core_data_provider;
 
 public class SingletonSqlConnection
 {
     private static SingletonSqlConnection _instance;
 
-    //public readonly 
+    private readonly SqlConnection connection;
 
-    private SingletonSqlConnection() { }
+    private SingletonSqlConnection()
+    {
+        connection = new SqlConnection();
+        connection.Open();
+    }
 
     public static SingletonSqlConnection Instance
     {
@@ -17,8 +24,48 @@ public class SingletonSqlConnection
         }
     }
 
+    public DataTable ExecuteQueryCommand(string sql)
+    {
+        DataTable dt = new DataTable();
+        OpenConnection();
+        SqlCommand command = new SqlCommand(sql, connection);
+        command.CommandType = CommandType.Text;
+        SqlDataReader reader = command.ExecuteReader();
+        dt.Load(reader);
+        command.Dispose();
+        return dt;
+    }
+    
+    public async Task<DataTable> ExecuteQueryCommandAsync(string sql)
+    {
+        DataTable dt = new DataTable();
+        OpenConnection();
+        SqlCommand command = new SqlCommand(sql, connection);
+        command.CommandType = CommandType.Text;
+        SqlDataReader reader = await command.ExecuteReaderAsync();
+        dt.Load(reader);
+        command.Dispose();
+        return dt;
+    }
+
+    public void OpenConnection()
+    {
+        if (connection.State == ConnectionState.Open) return;
+
+        connection.Open();
+    }
+
     public void CloseConnection()
     {
-        _instance.CloseConnection();
+        if (connection.State == ConnectionState.Closed) return;
+
+        connection.Close();
+    }
+
+    public void Dispose()
+    {
+        if (connection is null) return;
+
+        connection.Dispose();
     }
 }
